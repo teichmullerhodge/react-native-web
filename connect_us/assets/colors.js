72d2f529efbe535ea0d72774b14e80bad58ec626
Matheus <matheussilvatech@gmail.com>
2024-09-25 22:47:52 -0300

    creating web-view

---
+++ connect_us/assets/colors.js
@@ -0,0 +1,21 @@
+// src/colors.js
+const colors = {
+
+    dreamWhite: "#e9f4ff",
+    dreamPurple: "#6b76ff",
+    dreamBlue: "#065ad8",
+    dreamGreen: "#3C9C6C",
+    dreamRed: "#ff5757",
+    dreamYellow: "#ffaa00",
+    dreamBlack: "#1c1c3e",
+    dreamSmoke: "#f6f6f6",
+    iconcolor: "#979191",
+    dreamCyan: "#6bdddd",
+    dreamMagenta: "#f9a7a7",
+    opaqueBlue: "#e0ebfc",
+    dreamSalm: '#3762cc',
+    textColor: '#434e59',
+  };
+  
+  export default colors;
+  
